4007542b2718fa23c6d6ace9e2e24fb2a1f6b2ba
Ishak ates <ishakfeuer@gmail.com>
2026-01-16 21:08:28 +0100

    add projects and skillsets in key value pairs in an array and grep informations about the projects and skils from this object

--- src/components/Projects/projects.tsx
+++ src/components/Projects/projects.tsx
@@ -1,18 +1,52 @@
 import styles from './projects.module.css';
 
 const Projects = () => {
+    const projects = [
+        {
+            name: 'Baby Tools',
+            image: 'img/projectLogos/babyTools.png',
+            skills: [
+                { skillname: 'GitHub', image: 'img/skillLogos/github.png' },
+            ]
+        },
+        {
+            name: 'Truck Signs API',
+            image: 'img/projectLogos/truckSigns.png',
+            skills: [
+                { skillname: 'GitHub', image: 'img/skillLogos/github.png' },
+                { skillname: 'Docker', image: 'img/skillLogos/docker.png' },
+                { skillname: 'PostgreSQL', image: 'img/skillLogos/postgresql.png' },
+            ]
+        },
+        {
+            name: 'Juice Shop Meister',
+            image: 'img/projectLogos/juiceShop.png',
+            skills: [
+                { skillname: 'GitHub', image: 'img/skillLogos/github.png' },
+                { skillname: 'Docker', image: 'img/skillLogos/docker.png' },
+                { skillname: 'PostgreSQL', image: 'img/skillLogos/postgresql.png' },
+            ]
+        }
+    ];
+
   return (
     <section className={styles.projectsSection}>
         <div className={styles.mainContainer}>
             <h2 className={styles.projectsTitle}>My project highlights</h2>
             <div className={styles.projectsContainer}>
-                <div>
-                    <ul>
-                        <li>1. Baby Tools</li>
-                        <li>2. Truck Signs API</li>
-                        <li>3. Juice Shop Meister</li>
-                    </ul>
-                    <div>
+                <div className={styles.projectList}>
+                    {projects.length === 0 ? (
+                        <span>Projects will be added soon</span>
+                    ) : ( 
+                        projects.map((pro, index) => (
+                            <ul>
+                                <li key={index}>{index+1}. {pro.name}</li>
+                            </ul>
+                        ))
+                    )}
+                </div>
+                <div className={styles.projectCardsContainer}>
+                    <div className={styles.projectCard}>
                         <div className={styles.leftCt}>
                             <h3>Project Name</h3>
                             <img src="#" alt="Project Image" />
