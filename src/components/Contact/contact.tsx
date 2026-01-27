04adcc09f693ddb93f21d7c014b9e6508ed02c28
Ishak ates <ishakfeuer@gmail.com>
2026-01-27 17:08:16 +0100

    finisched contact section and add footer component

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -6,7 +6,27 @@ export default function Contact() {
         <section className={styles.contactSection}>
 
             <div className={styles.contactContainer}>
-                <h2 className={styles.contactTitle}>Contact me</h2>
+                <div className={styles.firstRow}>
+                    <h2 className={styles.contactTitle}>Contact me</h2>
+                    <span>Looking forward to hearing from you!</span>
+                </div>
+                <div className={styles.secondRow}>
+                    <div className={styles.contactDescription}>
+                        <p>I am seeking Mid-Level DevSecOps opportunities with a focus on containerized applications, secure infrastructure, and automated CI/CD pipelines. I contribute to teams by designing reproducible deployment workflows, improving system reliability, and integrating security best practices throughout the development lifecycle.</p>
+                        <p>I am open to full-time on-site, hybrid, or remote positions within international or EU-based teams. Feel free to reach out to discuss potential collaboration.</p>
+                    </div>
+                    <div className={styles.contactMethods}>
+                        <a href="mailto:contact@ishakates.com">
+                            <img src="img/mailIcon.png" alt="" />
+                            <span>contact@ishakates.com</span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/ishak-ates/" target="_blank" rel="noopener noreferrer">
+                            <img src="img/linkedinIcon.png" alt="" />
+                            <span>My LinkedIn</span>
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from '@site/src/components/Hero/hero';
 import Skills from '@site/src/components/Skills/skills';
 import Projects from '@site/src/components/Projects/projects';
 import Contact from '@site/src/components/Contact/contact';
+import Footer from '@site/src/components/Footer/footer';
 
 export default function Home(): JSX.Element {
   return (
@@ -16,6 +17,7 @@ export default function Home(): JSX.Element {
         <Skills />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </Layout>
   );
