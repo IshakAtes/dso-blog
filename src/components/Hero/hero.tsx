c477471ac11d7925d5ed7297f60803a5770d60a0
Ishak ates <ishakfeuer@gmail.com>
2026-02-03 13:41:17 +0100

    hero section responsive up to 600px

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -10,11 +10,15 @@ const Hero = () => {
             <h1 className={styles.heroName}>Ishak Ates</h1>
             <span className={styles.role}>DevSecOps Engineer</span>
 
+            {/* Dieses Bild ist nur auf Mobile sichtbar */}
+            <img className={styles.heroImageMobile} src="img/heroIshak.jpg" alt="hero-image" />
+
             <p className={styles.aboutMe}>I am a DevSecOps-focused developer with a strong interest in automation, security, and modern infrastructure. This portfolio documents my projects, technical experiments, and continuous learning journey in software development and operations.</p>
 
             <button className={styles.btn1}>Contact me</button>
         </div>
         <div className={styles.heroImageContainer}>
+            {/* Dieses Bild ist nur auf Desktop sichtbar */}
             <img className={styles.heroImage} src="img/heroIshak.jpg" alt="hero-image" />
         </div>
         
