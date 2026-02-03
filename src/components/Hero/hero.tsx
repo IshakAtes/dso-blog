2c581aa1871dec4575804b7d28be0c9217ce3c64
Ishak ates <ishakfeuer@gmail.com>
2026-02-03 18:44:59 +0100

    skills section responsive up to 800px

--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <h1 className={styles.heroName}>Ishak Ates</h1>
             <span className={styles.role}>DevSecOps Engineer</span>
 
-            {/* Dieses Bild ist nur auf Mobile sichtbar */}
+            {/* Mobile view */}
             <img className={styles.heroImageMobile} src="img/heroIshak.jpg" alt="hero-image" />
 
             <p className={styles.aboutMe}>I am a DevSecOps-focused developer with a strong interest in automation, security, and modern infrastructure. This portfolio documents my projects, technical experiments, and continuous learning journey in software development and operations.</p>
@@ -18,7 +18,7 @@ const Hero = () => {
             <button className={styles.btn1}>Contact me</button>
         </div>
         <div className={styles.heroImageContainer}>
-            {/* Dieses Bild ist nur auf Desktop sichtbar */}
+            {/* Desktop view */}
             <img className={styles.heroImage} src="img/heroIshak.jpg" alt="hero-image" />
         </div>
         
