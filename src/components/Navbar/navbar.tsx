35783f73c4ba2348cb555f46e89eae824e0a1a74
Ishak ates <ishakfeuer@gmail.com>
2026-02-04 16:46:07 +0100

    add mobile nav

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,28 +1,73 @@
 import { JSX } from 'react';
 import styles from './navbar.module.css'
 import './../../css/custom.css';
+import { useState } from 'react';
 
 export function Nav(): JSX.Element {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
   return (
-    <section className={`${styles.navbar} globalPadding`}>
-        <a href="./" className={styles.logo}>
-            <img src="img/ishak_logo.png" alt="" />
-            <span>AI</span>
-        </a>
-        <ul className={styles.navItems}>
-            <li>
-                <a href="docs/projects/overview">Docs</a>
-            </li>
-            {/* <li>
-                <a href="blog">Blog</a>
-            </li> */}
-            <li>
-                <a href="#contact-section">Contact</a>
-            </li>
-            <li>
-                <a target='blank' href="https://github.com/IshakAtes/dso-blog.git">GitHub</a>
-            </li>
-        </ul>
+    <section className={`${styles.sectionBackground} globalPadding`}>
+        <div className={styles.navbar}>
+            <a href="./" className={styles.logo}>
+                <img src="img/ishak_logo.png" alt="" />
+                <span>AI</span>
+            </a>
+            <ul className={styles.navItems}>
+                <li>
+                    <a href="docs/projects/overview">Docs</a>
+                </li>
+                {/* <li>
+                    <a href="blog">Blog</a>
+                </li> */}
+                <li>
+                    <a href="#contact-section">Contact</a>
+                </li>
+                <li>
+                    <a target='blank' href="https://github.com/IshakAtes/dso-blog.git">GitHub</a>
+                </li>
+            </ul>
+        </div>
+
+        <div className={styles.mobileNav}>
+            <a href="./" className={styles.logo}>
+                <img src="img/ishak_logo.png" alt="" />
+                <span>AI</span>
+            </a>
+
+            {/* Der Burger-Button */}
+            <div 
+                className={`${styles.burger} ${isOpen ? styles.active : ''}`} 
+                onClick={toggleMenu}
+            >
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+                <span className={styles.bar}></span>
+            </div>
+
+            {/* Die eigentliche Navigationsliste */}
+            <ul className={`${styles.navLinks} ${isOpen ? styles.open : ''}`}>
+                <li><a href="#home" onClick={() => setIsOpen(false)}>Home</a></li>
+                <li><a href="#about" onClick={() => setIsOpen(false)}>Über mich</a></li>
+                <li><a href="#contact" onClick={() => setIsOpen(false)}>Kontakt</a></li>
+            </ul>
+
+
+            {/* <ul className={styles.navItems}>
+                <li>
+                    <a href="docs/projects/overview">Docs</a>
+                </li>
+                <li>
+                    <a href="#contact-section">Contact</a>
+                </li>
+                <li>
+                    <a target='blank' href="https://github.com/IshakAtes/dso-blog.git">GitHub</a>
+                </li>
+            </ul> */}
+        </div>
     </section>
   );
 }
