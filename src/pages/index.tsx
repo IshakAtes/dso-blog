6c7718036bbf42fe3d8e61463d599ba119e52c94
Ishak ates <ishakfeuer@gmail.com>
2026-03-27 13:29:35 +0100

    rename all component files to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Layout from '@theme/Layout';
 import { JSX } from 'react';
-import { Nav } from '@site/src/components/Navbar/navbar';
-import Hero from '@site/src/components/Hero/hero';
-import Skills from '@site/src/components/Skills/skills';
-import Projects from '@site/src/components/Projects/projects';
-import Contact from '@site/src/components/Contact/contact';
-import Footer from '@site/src/components/Footer/footer';
+import { Nav } from '@site/src/components/Navbar';
+import Hero from '@site/src/components/Hero';
+import Skills from '@site/src/components/Skills';
+import Projects from '@site/src/components/Projects';
+import Contact from '@site/src/components/Contact';
+import Footer from '@site/src/components/Footer';
 
 export default function Home(): JSX.Element {
   return (
