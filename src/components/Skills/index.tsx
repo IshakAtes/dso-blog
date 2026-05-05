2208cd4825501d005f1d1d399ddbe263024d451a
Ishak ates <ishakfeuer@gmail.com>
2026-05-05 13:55:34 +0200

    add mobile skills section with carousel Skills-container but its not finished. part 1

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,7 +1,11 @@
+import { useState, useRef } from 'react';
+import type { TouchEvent } from 'react';
 import styles from './skills.module.css';
 import './../../css/custom.css';
 
 const Skills = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const startX = useRef(0);
   const skills = [
     {
       name: 'HTML',
@@ -104,33 +108,114 @@ const Skills = () => {
     },
   ];
 
+  // Skills in 3er Gruppen aufteilen
+  const groupedSkills = [];
+  for (let i = 0; i < skills.length; i += 3) {
+    groupedSkills.push(skills.slice(i, i + 3));
+  }
+
   return (
     <section id="skills-section" className={styles.skillsSection}>
-        <div className={`${styles.skillsContainer} globalPadding`}>
-            <h2 className={styles.skillsTitle}>My skills</h2>
-            <div className={styles.skillCardsContainer}>
-                {skills.map((skill, index) => (
-                  <div key={index} className={styles.skillCard}>
+      
+      {/* Desktop Version */}
+      <div className={`${styles.skillsContainer} globalPadding`}>
+        <h2 className={styles.skillsTitle}>My skills</h2>
+        <div className={styles.skillCardsContainer}>
+          {skills.map((skill, index) => (
+            <div key={index} className={styles.skillCard}>
+              <div className={styles.skillFront}>
+                <img
+                  className={styles.skillImage}
+                  src={skill.image}
+                  alt={skill.name}
+                />
+                <span>{skill.name}</span>
+              </div>
 
-                    <div className={styles.skillFront}>
-                      <img className={styles.skillImage} src={skill.image} alt={skill.name} />
+              <div className={styles.skillBack}>
+                <ul className={styles.skillList}>
+                  <span className={styles.skillListTitle}>
+                    How I used this skills
+                  </span>
+                  {skill.content.map((item, i) => (
+                    <li key={i}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Mobile Version */}
+      <div className={`${styles.mobileSkillsContainer} globalPadding`}>
+        <h2 className={styles.skillsTitle}>My skills</h2>
+
+        <div
+          className={styles.carousel}
+          onPointerDown={(e) => {
+            startX.current = e.clientX;
+          }}
+          onPointerUp={(e) => {
+            const diff = startX.current - e.clientX;
+
+            console.log("Pointer diff:", diff);
+
+            if (diff > 30 && currentIndex < groupedSkills.length - 1) {
+              setCurrentIndex((prev) => prev + 1);
+            }
+
+            if (diff < -30 && currentIndex > 0) {
+              setCurrentIndex((prev) => prev - 1);
+            }
+          }}
+        >
+          <div
+            className={styles.carouselTrack}
+            style={{
+              transform: `translateX(-${currentIndex * 100}%)`,
+            }}
+          >
+            {groupedSkills.map((group, groupIndex) => (
+              <div key={groupIndex} className={styles.slide}>
+                {group.map((skill, index) => (
+                  <div key={index} className={styles.mobileSkillCard}>
+                    <div className={styles.skillsMobileContainer}>
+                      <img
+                        className={styles.skillMobileImage}
+                        src={skill.image}
+                        alt={skill.name}
+                      />
                       <span>{skill.name}</span>
                     </div>
 
-                    <div className={styles.skillBack}>
-                      <ul className={styles.skillList}>
-                        <span className={styles.skillListTitle}>How I used this skills</span>
-                        {skill.content.map((item, i) => (
-                          <li key={i}>{item}</li>
-                        ))}
-                      </ul>
-                    </div>
+                    <ul className={styles.mobileSkillList}>
+                      {skill.content.map((item, i) => (
+                        <li key={i}>{item}</li>
+                      ))}
+                    </ul>
                   </div>
                 ))}
-            </div>
+              </div>
+            ))}
+          </div>
         </div>
+
+        {/* Dots */}
+        <div className={styles.dots}>
+          {groupedSkills.map((_, index) => (
+            <span
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+              className={`${styles.dot} ${
+                index === currentIndex ? styles.activeDot : ''
+              }`}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
-}
+};
 
 export default Skills;
